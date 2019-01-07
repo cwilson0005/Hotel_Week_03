2fd132aecf4645634a3fa019130811a3fe5c421f
Conor <cwilson0005@hotmail.co.uk>
2019-01-07 10:20:54 +0000

    Created necessary files

---
+++ specs/hotel_spec.rb
@@ -0,0 +1,9 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../hotel')
+
+class HotelTest < MiniTest::Test
+
+  
+
+end
---
+++ specs/room_spec.rb
@@ -0,0 +1,9 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../room')
+
+class RoomTest < MiniTest::Test
+
+
+
+end
